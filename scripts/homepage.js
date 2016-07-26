52d7fea4dd435f9872db905e7f87e8eb48da232e
Netlic <branislav.kutnik@gmail.com>
2016-07-26 21:40:37 +0200

    pridany header, doplnena render metoda pre polozku kategorie

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -2,13 +2,19 @@ var CategoryBox = React.createClass({
   displayName: 'Kategórie',
   render: function(){
     return (
-      <div className="categoryBox"></div>
+      <div className="categoryBox">
+        <h2>Kategórie</h2>
+      </div>
     )
   }
 });
 
 var CategoryItem = React.createClass({
-
+  render: function(){
+    return (
+      <div></div>
+    )
+  }
 });
 
 ReactDOM.render(
